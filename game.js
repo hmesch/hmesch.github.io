4eab60b3c73378b0e77e7081ac996dfb740c4363
Steffen Meschkat <mesch@google.com>
2019-08-02 14:46:42 +0200

    Split script and style into their own files.

---
+++ game.js
@@ -0,0 +1,64 @@
+
+var pos = {
+    x: 10,
+    y: 90
+};
+
+var dx = 0;
+var dy = 0;
+
+function step() {
+    if (pos.x > 1000 && dx > 0) {
+        dx = 0;
+    }
+    
+    if (pos.x < 0 && dx < 0) {
+        dx = 0;
+    }
+    
+    if (pos.y > 500 && dy > 0) {
+        dy = 0;
+    } 
+    
+    if (pos.y < 0 && dy < 0) {
+        dy = 0;
+    }
+    
+    pos.x += dx;
+    pos.y += dy;
+    
+    document.getElementById('x').style.top = pos.y + 'px';
+    document.getElementById('x').style.left = pos.x + 'px';
+}
+
+window.setInterval(step, 20);
+
+function keyup(event) {
+    if (event.keyCode == 37) {
+	    dx = 0;
+    }
+    if (event.keyCode == 38) {
+	    dy = 0;
+    }
+    if (event.keyCode == 39) {
+	    dx = 0;
+    }
+    if (event.keyCode == 40) {
+	    dy = 0;
+    }      
+}
+
+function keydown(event) {
+    if (event.keyCode == 37) {
+	    dx = -1;
+    }
+    if (event.keyCode == 38) {
+	    dy = -1;
+    }
+    if (event.keyCode == 39) {
+	    dx = 1;
+    }
+    if (event.keyCode == 40) {
+	    dy = 1;
+    }
+}
