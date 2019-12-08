4ae0ecf6ded552c6c8f7ee0305931e44c4ab043d
Steffen Meschkat <mesch@google.com>
2019-12-08 15:53:24 -0800

    * define rocks on the field, initially random * display rocks * prevent mark from running over rocks when it moves * setup rocks only after onload event, in new function init().
    TODO:
    * computation of which rock we are at is off by one pixel.

--- game.js
+++ game.js
@@ -1,45 +1,104 @@
-var pos = {
-    x: 10,
-    y: 90
-};
-
 var dx = 0;
 var dy = 0;
+
+var field = [];
+
+function init() {
+    var I = Math.ceil(document.getElementById('field').clientWidth /
+                      document.getElementById('x').clientWidth);
+    var J = Math.ceil(document.getElementById('field').clientHeight /
+                      document.getElementById('x').clientHeight);
+
+    for (var i = 0; i < I; ++i) {
+        field.push([]);
+        for (var j = 0; j < J; ++j) {
+            if (Math.random() > 0.9) {
+                field[i].push(1);
+            } else {
+                field[i].push(0);
+            }
+        }
+    }
+    
+    for (var i = 0; i < field.length; ++i) {
+        for (var j = 0; j < field[i].length; ++j) {
+            if (field[i][j] == 1) {
+                var e = document.createElement('div');
+                e.className = 'rock';
+                e.style.top = (j * 10) + 'px';
+                e.style.left = (i * 10) + 'px';
+                document.getElementById('field').appendChild(e);
+            }
+        }
+    }
+    
+    window.setInterval(step, 5);
+}
 
 function step() {
     if (dx == 0 && dy == 0) {
         return;
     }
-    
-    var xmax = document.getElementById('field').clientWidth -
-        document.getElementById('x').offsetWidth;
-    var ymax = document.getElementById('field').clientHeight -
-        document.getElementById('x').offsetHeight;
 
-    if (pos.x > xmax && dx > 0) {
-        dx = 0;
+    var xsize = document.getElementById('field').clientWidth;
+    var ysize = document.getElementById('field').clientHeight;
+
+    var xm = document.getElementById('x').clientWidth
+    var ym = document.getElementById('x').clientHeight
+    
+    var xmax = xsize - document.getElementById('x').offsetWidth;
+    var ymax = ysize - document.getElementById('x').offsetHeight;
+
+    var posx = document.getElementById('x').offsetLeft;
+    var posy = document.getElementById('x').offsetTop;
+
+    var i = Math.floor(posx / xm);
+    var j = Math.floor(posy / ym);
+    
+    if (dx > 0) {
+        if (posx > xmax) {
+            dx = 0;
+        }
+
+        if (field[i+1][j] == 1) {
+            dx = 0;
+        }
+        
+    } else if (dx < 0) {
+        if (posx < 0) {
+            dx = 0;
+        }
+
+        if (field[i][j] == 1) {
+            dx = 0;
+        }
+    }
+
+    if (dy > 0) {
+        if (posy > ymax) {
+            dy = 0;
+        }
+
+        if (field[i][j+1] == 1) {
+            dy = 0;
+        }
+
+    } else if (dy < 0) {
+        if (posy < 0) {
+            dy = 0;
+        }
+
+        if (field[i][j] == 1) {
+            dy = 0;
+        }
     }
     
-    if (pos.x < 0 && dx < 0) {
-        dx = 0;
-    }
+    posx += dx;
+    posy += dy;
     
-    if (pos.y > ymax && dy > 0) {
-        dy = 0;
-    } 
-    
-    if (pos.y < 0 && dy < 0) {
-        dy = 0;
-    }
-    
-    pos.x += dx;
-    pos.y += dy;
-    
-    document.getElementById('x').style.top = pos.y + 'px';
-    document.getElementById('x').style.left = pos.x + 'px';
+    document.getElementById('x').style.top = posy + 'px';
+    document.getElementById('x').style.left = posx + 'px';
 }
-
-window.setInterval(step, 5);
 
 function keyup(event) {
     if (event.keyCode == 37) {
@@ -78,3 +137,4 @@ function keydown(event) {
         event.preventDefault();
     }
 }
+
