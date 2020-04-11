a556fd22fc67eaeb645fdf5a46f8e0130500ed74
Helene Meschkat <helene@meschkat.de>
2020-04-10 17:29:06 -0700

    formatting: tab -> space

--- game.js
+++ game.js
@@ -128,38 +128,38 @@ function step() {
 
 function keyup(event) {
     if (event.keyCode == 37) {
-	    dx = 0;
+        dx = 0;
         event.preventDefault();
     }
     if (event.keyCode == 38) {
-	    dy = 0;
+        dy = 0;
         event.preventDefault();
     }
     if (event.keyCode == 39) {
-	    dx = 0;
+        dx = 0;
         event.preventDefault();
     }
     if (event.keyCode == 40) {
-	    dy = 0;
+        dy = 0;
         event.preventDefault();
     }      
 }
 
 function keydown(event) {
     if (event.keyCode == 37) {
-	    dx = -1;
+        dx = -1;
         event.preventDefault();
     }
     if (event.keyCode == 38) {
-	    dy = -1;
+        dy = -1;
         event.preventDefault();
     }
     if (event.keyCode == 39) {
-	    dx = 1;
+        dx = 1;
         event.preventDefault();
     }
     if (event.keyCode == 40) {
-	    dy = 1;
+        dy = 1;
         event.preventDefault();
     }
 }
